4afd434e557c4d7803f3fbad0ae79a7e552c605c
Sabit hasan <sabithasan2098@gmail.com>
2024-08-15 16:52:40 +0600

    create authProvider context set .env

--- src/main.jsx
+++ src/main.jsx
@@ -4,13 +4,16 @@ import { RouterProvider } from "react-router-dom";
 import { router } from "./router/Router";
 import { HelmetProvider } from "react-helmet-async";
 import "./index.css";
+import AuthProvider from "./Provider/AuthProvider";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <HelmetProvider>
-      <div className="max-w-screen-lg mx-auto">
-        <RouterProvider router={router} />
-      </div>
-    </HelmetProvider>
+    <AuthProvider>
+      <HelmetProvider>
+        <div className="max-w-screen-lg mx-auto">
+          <RouterProvider router={router} />
+        </div>
+      </HelmetProvider>
+    </AuthProvider>
   </StrictMode>
 );
