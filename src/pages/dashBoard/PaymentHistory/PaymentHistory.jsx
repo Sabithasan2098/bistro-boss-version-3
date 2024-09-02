f3955a6c890c65dbace1f12a1d353f146922d60b
sabit <sabithasan2098@gmail.com>
2024-09-02 13:01:09 +0600

    set the the payment history table & when payment successfull auto redirect to the redirect page

--- src/pages/dashBoard/PaymentHistory/PaymentHistory.jsx
+++ src/pages/dashBoard/PaymentHistory/PaymentHistory.jsx
@@ -1,7 +1,62 @@
+import { useQuery } from "@tanstack/react-query";
+import useAuth from "../../../hooks/useAuth";
+import useAxios from "../../../hooks/useAxios";
+import SectionTitle from "../../../components/SectionTitle/SectionTitle";
+
 const PaymentHistory = () => {
+  const { user } = useAuth();
+  const axiosSecure = useAxios();
+
+  const {
+    data: payments,
+    isLoading,
+    error,
+  } = useQuery({
+    queryKey: ["payments", user?.email], // Include email in the query key to refetch if the email changes
+    queryFn: async () => {
+      const res = await axiosSecure.get(`/payments/${user.email}`);
+      return res.data;
+    },
+  });
+
+  if (error) return <div>Error fetching payments</div>;
+
   return (
     <div>
-      <h1>Its the payment history page</h1>
+      <SectionTitle
+        subHeading={"At a Glance!"}
+        heading={"payment history"}
+      ></SectionTitle>
+      {isLoading ? (
+        <span className="loading loading-bars loading-xs"></span>
+      ) : (
+        <div className="overflow-x-auto">
+          <table className="table">
+            {/* head */}
+            <thead className="bg-[#d1a054] text-white">
+              <tr>
+                <th>Email</th>
+                <th>Tranjection Id</th>
+                <th>Total Price</th>
+                <th>Payment date</th>
+                <th>Status</th>
+              </tr>
+            </thead>
+            <tbody>
+              {/* row 1 */}
+              {payments?.map((payment) => (
+                <tr key={payment._id}>
+                  <th>{payment?.email}</th>
+                  <td>{payment?.transectionId}</td>
+                  <td>{payment?.price}</td>
+                  <td>{new Date(payment?.date).toLocaleDateString()}</td>
+                  <td>{payment.status}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      )}
     </div>
   );
 };
